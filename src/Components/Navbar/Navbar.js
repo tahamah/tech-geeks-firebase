ba3348c8238d36b0e72207ab348212de51757b4a
unknown <taha.iu.bd@gmail.com>
2022-04-11 11:28:16 +0600

    optional randaring add

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,43 +1,67 @@
-import React, { useEffect, useState } from "react";
-import { NavLink } from "react-router-dom";
-import Logo from "../../Assets/Image/logo.png";
-import "./Navbar.css";
-import { useLocation } from "react-router-dom";
+import React, { useEffect, useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import Logo from '../../Assets/Image/logo.png'
+import './Navbar.css'
+import { useLocation } from 'react-router-dom'
+import { auth } from '../../Firebase/Firebase.init'
+import { onAuthStateChanged } from 'firebase/auth'
 
 const Navbar = () => {
-  const { pathname } = useLocation();
+    const { pathname } = useLocation()
+    const [user, setUser] = useState({})
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setUser(user)
+            } else {
+                setUser({})
+            }
+        })
+    }, [])
 
-  return (
-    <nav
-      style={
-        pathname.includes("blog") ? { display: "none" } : { display: "flex" }
-      }
-    >
-      <div className='logo-container'>
-        <img src={Logo} alt='' />
-      </div>
-      <div className='link-container'>
-        <NavLink
-          className={({ isActive }) => (isActive ? "active-link" : "link")}
-          to='/'
+    return (
+        <nav
+            style={
+                pathname.includes('blog')
+                    ? { display: 'none' }
+                    : { display: 'flex' }
+            }
         >
-          Home
-        </NavLink>
-        <NavLink
-          className={({ isActive }) => (isActive ? "active-link" : "link")}
-          to='/videos'
-        >
-          Videos
-        </NavLink>
-        <NavLink
-          className={({ isActive }) => (isActive ? "active-link" : "link")}
-          to='/login'
-        >
-          Login
-        </NavLink>
-      </div>
-    </nav>
-  );
-};
+            <div className="logo-container">
+                <img src={Logo} alt="" />
+            </div>
+            <div className="link-container">
+                <NavLink
+                    className={({ isActive }) =>
+                        isActive ? 'active-link' : 'link'
+                    }
+                    to="/"
+                >
+                    Home
+                </NavLink>
+                <NavLink
+                    className={({ isActive }) =>
+                        isActive ? 'active-link' : 'link'
+                    }
+                    to="/videos"
+                >
+                    Videos
+                </NavLink>
+                <NavLink
+                    className={({ isActive }) =>
+                        isActive ? 'active-link' : 'link'
+                    }
+                    to="/login"
+                >
+                    {!user ? (
+                        'Log in'
+                    ) : (
+                        <button className="logout-button">Log Out</button>
+                    )}
+                </NavLink>
+            </div>
+        </nav>
+    )
+}
 
-export default Navbar;
+export default Navbar
